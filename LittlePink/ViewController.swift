bf3bb53644d5662b28fe2f61594e8fba80048f8a
v_mayaqiong <v_mayaqiong@baidu.com>
2022-04-01 18:23:20 +0800

    Initial Commit

---
+++ LittlePink/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LittlePink
+//
+//  Created by v_mayaqiong on 4/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
